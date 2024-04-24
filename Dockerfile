d20aef46db7f580ddf267c1f659e173096b0f6fd
Alex Pendell <alex.pendell@gmail.com>
2024-04-24 14:46:32 -0400

    Added new index, 'market_index', that will prompt the user for a stock symbol, and a drop down menu for which market to query. Submitting will try to grab the data from google finance pertaining the price of the stock on the index market. Uses some new stuff (selenium and chromium) for grabbing the html from google's site, and then uses JSoup to parse the html and check for the 'data-last-price' attribute within the html divs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM eclipse-temurin:22-jdk-alpine
 WORKDIR /app
+RUN apk add --no-cache chromium chromium-chromedriver
 COPY .mvn/ .mvn
 COPY mvnw pom.xml ./
 RUN ./mvnw dependency:go-offline
--- src/main/java/com/justcode/alex/CRUDApp/Controller/ApiControllers.java
+++ src/main/java/com/justcode/alex/CRUDApp/Controller/ApiControllers.java
@@ -1,7 +1,16 @@
 package com.justcode.alex.CRUDApp.Controller;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.justcode.alex.CRUDApp.Models.User;
@@ -29,21 +39,60 @@ public class ApiControllers {
     
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "market_index";
+    }
+
+    /** userSearch(String search_string)
+     * @param symbol the symbol to search for 
+     * @param index the market where the symbol exists
+     * @return the last price of the symbol (per https://www.google.com/finance/quote/${symbol}:${index})
+     * @throws IOException 
+     */
+    @ResponseBody @GetMapping("/market_lookup")
+    public String getPrice(@RequestParam("search_symbol") String symbol, @RequestParam("market_index") String index) throws IOException {
+        
+        // Uppercase the market and symbol
+        symbol = symbol.toUpperCase();
+        index = index.toUpperCase();
+
+        // Sanity check the input
+        System.out.println("Looking for SYMBOL " + symbol);
+        System.out.println("... on the " + index + " market.");
+        System.out.println("Trying to get market price from https://www.google.com/finance/quote/"+symbol+":"+index);
+
+        // Setup browsing options to run chromium/chrome in a docker container
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+
+        // Spin up the web driver and get the html
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/finance/quote/"+symbol+":"+index);
+
+        // Parse the html, looking for the 'data-last-price' attribute
+        // This is the attribute that holds google's last known price of the symbol:index
+        Document doc = Jsoup.parse(driver.getPageSource());
+        Element div = doc.select("div[data-last-price]").first();
+        String price = div.attr("data-last-price");
+
+        return symbol + " : " + index + " = " + price;
     }
 
     /** userSearch(String search_string)
      * @param search_string the string to crawl the web searching for
      * @return ... the same string at the moment, happy to be getting
      *             information from the html/javascript page
+     * @throws IOException 
      */
     @ResponseBody @PostMapping("/search")
-    public String userSearch(@RequestBody String search_string) {
+    public String userSearch(@RequestBody String search_string) throws IOException {
         
+        Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
         return "Hello, you searched for " + search_string.split("=")[1];
     }
 
-    /*
+    /**
      * The functions below are the basic CRUD functions. Currently they're
      * not really important, as this is shaping up to be a search engine.
      * HOWEVER, it would be good to save search_string's (from the func above)
